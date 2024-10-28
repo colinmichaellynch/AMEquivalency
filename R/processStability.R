de11f28e12652292b07cfcbcf25f0b86643c0760
Colin Lynch <61156429+colinmichaellynch@users.noreply.github.com>
2024-10-28 15:48:42 -0700

    Update processStability.R

--- R/processStability.R
+++ R/processStability.R
@@ -1,14 +1,17 @@
 #' Check Process Stability
 #'
-#' This function generates an x-bar chart of the variable measured from the reference process. 
-#' It also counts the number of points that fall outside of the control limits.
-#' Data must be in the following form to work: it must have n rows (number of observations per subgroup) and m columns (number of subgroups) where the columns are in chronological order of measurement. Each cell must contain a numeric value. 
-#' @param data values derived from chronological experiment
-#' @return The number of out of control points and x-bar chart
+#' This function generates an x-bar chart of the variable measured from the reference process and counts the number of out of control points.
+#' @section Details:
+#' Data must be in the following form to work: it must have n rows (number of observations per subgroup) and m columns (number of subgroups) where the columns are in chronological order of measurement. Each cell must contain a numeric value.
+#' @param data values derived from chronological experiment.
+#' @return
+#'   \describe{
+#'     \item{OOC}{The number of out of control points and x-bar chart.}
+#'    }
 #' @export
 
 processStability = function(data, show.plot = TRUE) {
-  
+
   xBar = colMeans(data)
   xBarBar = mean(xBar)
   R = apply(data, 2, max)-apply(data, 2, min)
@@ -33,17 +36,19 @@ processStability = function(data, show.plot = TRUE) {
   } else if(n == 10){
     A2 = .31
   }
-  
+
   LCL = xBarBar - A2*RBar
   UCL = xBarBar + A2*RBar
-  
+
   if(show.plot){
     plt = plot(xBar, xlab = "Time Order", ylab = "X Bar", main = "Control Chart", ylim = c(min(c(xBar, LCL)) - (min(c(xBar, LCL))*.05), max(c(xBar, UCL)) + (max(c(xBar, UCL))*.05)), pch = 19)
     abline(h = LCL, lty = 2, lwd = 2)
     abline(h = UCL, lty = 2, lwd = 2)
     abline(h = xBarBar)
   }
-  
-  return(sum(xBar < LCL | xBar > UCL))
-  
+
+  OOC = sum(xBar < LCL | xBar > UCL)
+
+  return(OOC)
+
 }
