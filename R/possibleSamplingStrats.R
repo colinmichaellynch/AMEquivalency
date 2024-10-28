584d77315fb98f4385537fda971b6f4a3e50ba37
Colin Lynch <61156429+colinmichaellynch@users.noreply.github.com>
2024-10-28 15:48:15 -0700

    Update possibleSamplingStrats.R

--- R/possibleSamplingStrats.R
+++ R/possibleSamplingStrats.R
@@ -1,34 +1,39 @@
 #' Set of Possible Designs
 #'
-#' This function finds all of the possible sampling strategies whose final sample size is equal to a constant set by the user (N_1T_Max). 
+#' This function finds all of the possible sampling strategies whose final sample size is equal to a constant set by the user (N_1T_Max).
 #' @param N_1T_Max final sample size of experiment given the reference and candidate processes are equivalent
 #' @return List of N_1 and T values which produce strategies whose final sample size is equal to N_1T_Max
+#' @return A list containing pairs of values which determine all of the sampling strategy whose final sample size is equal to N_1T_Max.
+#'   \describe{
+#'     \item{TVecAcceptable}{A numeric vector containing all values of T.}
+#'     \item{n1VecAcceptable}{A numeric vector containing all values of N1.}
+#'   }
 #' @export
 
 possibleSamplingStrats = function(N_1T_Max=40){
- 
+
   TVecAcceptable = c()
   n1VecAcceptable = c()
   counter = 0
   for(n_1 in 1:N_1T_Max){
-    
+
     for(T in 1:40){
-      
+
       s = (1-n_1)/T
       n_t = round(((1:T)*n_1)+((((1:T)-1)*((1:T)+2))/2)*s)
-      
+
       if(n_t[T] == N_1T_Max){
-        
+
         counter = counter + 1
         TVecAcceptable[counter] = T
         n1VecAcceptable[counter] = n_1
-        
+
       }
-      
+
     }
-    
+
   }
-  
+
   return(list(TVecAcceptable, n1VecAcceptable))
-   
+
 }
